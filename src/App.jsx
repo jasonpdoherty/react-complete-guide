f03fb3b74a95b7ee52ed044db72da5feaf2a0089
Jason Doherty <jason.doherty@activeops.com>
2018-08-30 13:34:19 +0100

    S4, L54 - Lists and State management

--- src/App.jsx
+++ src/App.jsx
@@ -12,16 +12,6 @@ class App extends Component {
         showPersons: false
     };
 
-    switchNameHandler = (newName) => {
-        this.setState({
-            persons: [
-                { name: newName, age: 39 },
-                { name: 'Jack', age: 7 },
-                { name: 'Harry', age: 4 }
-            ]
-        });
-    };
-
     namedChangedHandler = (event) => {
         this.setState({
             persons: [
@@ -30,6 +20,12 @@ class App extends Component {
                 { name: 'Harry', age: 4 }
             ]
         });
+    }
+
+    deletePersonHandler = (personIndex) => {
+        const persons = this.state.persons;
+        persons.splice(personIndex, 1);
+        this.setState({ persons: persons });
     }
 
     togglePersonHandler = () => {
@@ -52,8 +48,8 @@ class App extends Component {
         if (this.state.showPersons) {
             persons = (
                 <div>
-                    {this.state.persons.map(person => {
-                        return <Person name={person.name} age={person.age}/>;
+                    {this.state.persons.map((person, index) => {
+                        return <Person name={person.name} age={person.age} click={() => this.deletePersonHandler(index)}/>;
                     })}
             </div>);
         }
--- src/Person/person.jsx
+++ src/Person/person.jsx
@@ -5,7 +5,7 @@ const person = (props) => {
     return (
         <div className="Person">
             <p onClick={props.click}>I'm {props.name} and I am {props.age}</p>
-            <p onClick={props.click}>{props.children}</p>
+            <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name}/>
         </div>
 )};
