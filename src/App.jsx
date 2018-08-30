44f053c2fb618bd932b9e0b202b371c625236c38
Jason Doherty <jason.doherty@activeops.com>
2018-08-30 12:59:24 +0100

    S4, L51 - alternative and more elegant conditional content basics

--- src/App.jsx
+++ src/App.jsx
@@ -47,26 +47,28 @@ class App extends Component {
             cursor: 'pointer'
         };
 
+        let persons = null;
+
+        if (this.state.showPersons) {
+            persons = (<div>
+                <Person
+                    name={this.state.persons[0].name}
+                    age={this.state.persons[0].age} />
+                <Person
+                    name={this.state.persons[1].name} changed={this.namedChangedHandler}
+                    age={this.state.persons[1].age} click={this.switchNameHandler.bind(this, 'Lorna!')}>Hobbies : football</Person>
+                <Person
+                    name={this.state.persons[2].name}
+                    age={this.state.persons[2].age} />
+            </div>);
+        }
+
         return (
             <div className="App">
                 <h1>Hi, I'm a react App</h1>
                 <p>This is really working</p>
-                <button
-                    style={style}
-                    onClick={this.togglePersonHandler}>Switch</button>
-                    {this.state.showPersons ?
-                    <div>
-                        <Person
-                            name={this.state.persons[0].name}
-                            age={this.state.persons[0].age} />
-                        <Person
-                            name={this.state.persons[1].name} changed={this.namedChangedHandler}
-                            age={this.state.persons[1].age} click={this.switchNameHandler.bind(this, 'Lorna!')}>Hobbies : football</Person>
-                        <Person
-                            name={this.state.persons[2].name}
-                            age={this.state.persons[2].age} />
-                    </div> : null
-                    }
+                <button style={style} onClick={this.togglePersonHandler}>Switch</button>
+                {persons}
             </div>
         );
     }
