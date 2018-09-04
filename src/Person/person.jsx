956380be5ad0b3a6bec51aa39b709c0053eed54a
Jason Doherty <jason.doherty@activeops.com>
2018-09-04 15:53:45 +0100

    S5, L66 - CSS modules

--- src/Person/person.jsx
+++ src/Person/person.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import './person.css'
+import classes from './person.css'
 
 
 const person = (props) => {
         return (
-        <div className="Person">
+        <div className={classes.Person}>
             <p onClick={props.click}>I'm {props.name} and I am {props.age}</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name}/>
